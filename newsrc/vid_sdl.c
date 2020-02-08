5228357e49aa0141fb42363cabc17c97591f40d6
James Anderson <caracallarex@gmail.com>
2020-02-07 17:51:56 -0800

    input and video logic cleanup

--- newsrc/vid_sdl.c
+++ newsrc/vid_sdl.c
@@ -35,6 +35,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "quakedef.h"
 #include "d_local.h"
 
+
 cvar_t m_filter = {"m_filter", "0", true};  // mouse smoothing?
 
 qboolean mouse_avail;  // checks if mouse has been inited
@@ -48,6 +49,11 @@ float mouse_y;
 float old_mouse_x;
 float old_mouse_y;
 
+// for when the display resolution changes
+int config_notify = 0;
+int config_notify_width;
+int config_notify_height;
+
 viddef_t vid;  // global video state
 unsigned short d_8to16table[256];
 
@@ -60,7 +66,6 @@ int vid_buffersize;
 SDL_Window *window;
 SDL_Renderer *renderer;
 SDL_Texture *textureSDL;
-// SDL_Surface *screenSurface;
 SDL_Event event;
 
 static qboolean oktodraw = false;
@@ -81,7 +86,7 @@ typedef uint32_t PIXEL24;
 #define PIXEL_BUFFER_SIZE RES_X * RES_Y * sizeof(PIXEL24)
 
 typedef struct {
-	char data[PIXEL_BUFFER_SIZE];
+	unsigned char data[PIXEL_BUFFER_SIZE];
 	int bytes_per_line;
 } pixeldata;
 
@@ -98,19 +103,19 @@ static unsigned long b_mask;
 
 void shiftmask_init() {
 	unsigned int x;
-	r_mask = 0xFF0000; // x_vis->red_mask;
-	g_mask = 0xFF00; // x_vis->green_mask;
-	b_mask = 0xFF; // x_vis->blue_mask;
+	r_mask = 0xFF0000;
+	g_mask = 0xFF00;
+	b_mask = 0xFF;
 
-	for (r_shift =- 8, x = 1; x < r_mask; x = x << 1) {
+	for (r_shift = -8, x = 1; x < r_mask; x = x << 1) {
 		r_shift++;
 	}
 
-	for (g_shift =- 8, x = 1; x < g_mask; x = x << 1) {
+	for (g_shift = -8, x = 1; x < g_mask; x = x << 1) {
 		g_shift++;
 	}
 
-	for (b_shift =- 8, x = 1; x < b_mask; x = x << 1) {
+	for (b_shift = -8, x = 1; x < b_mask; x = x << 1) {
 		b_shift++;
 	}
 
@@ -164,11 +169,6 @@ void st3_fixup(int x, int y, int width, int height) {
 		return;
 	}
 
-	// x: 0
-	// y: 0
-	// width: 640
-	// height: 480
-
 	for (yi = y; yi < (y + height); yi++) {
 		src = &pixel_buffer.data[yi * pixel_buffer.bytes_per_line];
 
@@ -191,6 +191,10 @@ void st3_fixup(int x, int y, int width, int height) {
 		}
 	}
 }
+
+// ========================================================================
+// SDL Video Logic
+// ========================================================================
 
 void ResetFrameBuffer(void) {
 	if (d_pzbuffer) {
@@ -288,8 +292,6 @@ void VID_ShiftPalette(unsigned char *p) {
 	VID_SetPalette(p);
 }
 
-
-
 void VID_SetPalette(unsigned char *palette) {
 	int i;
 
@@ -299,388 +301,6 @@ void VID_SetPalette(unsigned char *palette) {
 				palette[i * 3 + 1],
 				palette[i * 3 + 2]);
 	}
-}
-
-void VID_Shutdown(void) {
-	Con_Printf("VID_Shutdown\n");
-	SDL_Quit();
-}
-
-#if 0
-int XLateKey(XKeyEvent *ev) {
-	int key;
-	char buf[64];
-	KeySym keysym;
-
-	key = 0;
-
-	XLookupString(ev, buf, sizeof buf, &keysym, 0);
-
-	switch(keysym) {
-		case XK_KP_Page_Up:
-		case XK_Page_Up:
-			key = K_PGUP;
-			break;
-
-		case XK_KP_Page_Down:
-		case XK_Page_Down:
-			key = K_PGDN;
-			break;
-
-		case XK_KP_Home:
-		case XK_Home:
-			key = K_HOME;
-			break;
-
-		case XK_KP_End:
-		case XK_End:
-			key = K_END;
-			break;
-
-		case XK_KP_Left:
-		case XK_Left:
-			key = K_LEFTARROW;
-			break;
-
-		case XK_KP_Right:
-		case XK_Right:
-			key = K_RIGHTARROW;
-			break;
-
-		case XK_KP_Down:
-		case XK_Down:
-			key = K_DOWNARROW;
-			break;
-
-		case XK_KP_Up:
-		case XK_Up:
-			key = K_UPARROW;
-			break;
-
-		case XK_Escape:
-			key = K_ESCAPE;
-			break;
-
-		case XK_KP_Enter:
-		case XK_Return:
-			key = K_ENTER;
-			break;
-
-		case XK_Tab:
-			key = K_TAB;
-			break;
-
-		case XK_F1:
-			key = K_F1;
-			break;
-
-		case XK_F2:
-			key = K_F2;
-			break;
-
-		case XK_F3:
-			key = K_F3;
-			break;
-
-		case XK_F4:
-			key = K_F4;
-			break;
-
-		case XK_F5:
-			key = K_F5;
-			break;
-
-		case XK_F6:
-			key = K_F6;
-			break;
-
-		case XK_F7:
-			key = K_F7;
-			break;
-
-		case XK_F8:
-			key = K_F8;
-			break;
-
-		case XK_F9:
-			key = K_F9;
-			break;
-
-		case XK_F10:
-			key = K_F10;
-			break;
-
-		case XK_F11:
-			key = K_F11;
-			break;
-
-		case XK_F12:
-			key = K_F12;
-			break;
-
-		case XK_BackSpace:
-			key = K_BACKSPACE;
-			break;
-
-		case XK_KP_Delete:
-		case XK_Delete:
-			key = K_DEL;
-			break;
-
-		case XK_Pause:
-			key = K_PAUSE;
-			break;
-
-		case XK_Shift_L:
-		case XK_Shift_R:
-			key = K_SHIFT;
-			break;
-
-		case XK_Execute:
-		case XK_Control_L:
-		case XK_Control_R:
-			key = K_CTRL;
-			break;
-
-		case XK_Alt_L:
-		case XK_Meta_L:
-		case XK_Alt_R:
-		case XK_Meta_R:
-			key = K_ALT;
-			break;
-
-		case XK_KP_Begin:
-			key = K_AUX30;
-			break;
-
-		case XK_Insert:
-		case XK_KP_Insert:
-			key = K_INS;
-			break;
-
-		case XK_KP_Multiply:
-			key = '*';
-			break;
-		case XK_KP_Add:
-			key = '+';
-			break;
-		case XK_KP_Subtract:
-			key = '-';
-			break;
-		case XK_KP_Divide:
-			key = '/';
-			break;
-
-#if 0
-		case 0x021:
-			key = '1';
-			break;/* [!] */
-
-		case 0x040:
-			key = '2';
-			break;/* [@] */
-
-		case 0x023:
-			key = '3';
-			break;/* [#] */
-
-		case 0x024:
-			key = '4';
-			break;/* [$] */
-
-		case 0x025:
-			key = '5';
-			break;/* [%] */
-
-		case 0x05e:
-			key = '6';
-			break;/* [^] */
-
-		case 0x026:
-			key = '7';
-			break;/* [&] */
-
-		case 0x02a:
-			key = '8';
-			break;/* [*] */
-
-		case 0x028:
-			key = '9';;
-			break;/* [(] */
-
-		case 0x029:
-			key = '0';
-			break;/* [)] */
-
-		case 0x05f:
-			key = '-';
-			break;/* [_] */
-
-		case 0x02b:
-			key = '=';
-			break;/* [+] */
-
-		case 0x07c:
-			key = '\'';
-			break;/* [|] */
-
-		case 0x07d:
-			key = '[';
-			break;/* [}] */
-
-		case 0x07b:
-			key = ']';
-			break;/* [{] */
-
-		case 0x022:
-			key = '\'';
-			break;/* ["] */
-
-		case 0x03a:
-			key = ';';
-			break;/* [:] */
-
-		case 0x03f:
-			key = '/';
-			break;/* [?] */
-
-		case 0x03e:
-			key = '.';
-			break;/* [>] */
-
-		case 0x03c:
-			key = ',';
-			break;/* [<] */
-
-#endif
-
-		default:
-			key = *(unsigned char*)buf;
-
-			if (key >= 'A' && key <= 'Z') {
-				key = key - 'A' + 'a';
-			}
-
-			// fprintf(stdout, "case 0x0%x: key = ___;break;/* [%c] */\n", keysym);
-			break;
-	}
-
-	return key;
-}
-#endif
-
-struct {
-	int key;
-	int down;
-} keyq[64];
-
-int keyq_head = 0;
-int keyq_tail = 0;
-
-int config_notify = 0;
-int config_notify_width;
-int config_notify_height;
-
-void GetEvent(void) {
-	// XEvent x_event;
-	// int b;
-	//
-	// XNextEvent(x_disp, &x_event);
-	//
-	// switch(x_event.type) {
-	// 	case KeyPress:
-	// 		keyq[keyq_head].key = XLateKey(&x_event.xkey);
-	// 		keyq[keyq_head].down = true;
-	// 		keyq_head = (keyq_head + 1) & 63;
-	// 		break;
-	//
-	// 	case KeyRelease:
-	// 		keyq[keyq_head].key = XLateKey(&x_event.xkey);
-	// 		keyq[keyq_head].down = false;
-	// 		keyq_head = (keyq_head + 1) & 63;
-	// 		break;
-	//
-	// 	case MotionNotify:
-	// 		mouse_x = (float) ((int)x_event.xmotion.x - (int)(vid.width / 2));
-	// 		mouse_y = (float) ((int)x_event.xmotion.y - (int)(vid.height / 2));
-	//
-	// 		/* move the mouse to the window center again */
-	// 		XSelectInput(
-	// 				x_disp,
-	// 				x_win,
-	// 				StructureNotifyMask|
-	// 						KeyPressMask|
-	// 						KeyReleaseMask|
-	// 						ExposureMask|
-	// 						ButtonPressMask|
-	// 						ButtonReleaseMask);
-	// 		XWarpPointer(
-	// 				x_disp,
-	// 				None,
-	// 				x_win,
-	// 				0,
-	// 				0,
-	// 				0,
-	// 				0,
-	// 				(vid.width / 2),
-	// 				(vid.height / 2));
-	// 		XSelectInput(
-	// 				x_disp,
-	// 				x_win,
-	// 				StructureNotifyMask|
-	// 						KeyPressMask|
-	// 						KeyReleaseMask|
-	// 						ExposureMask|
-	// 						PointerMotionMask|
-	// 						ButtonPressMask|
-	// 						ButtonReleaseMask);
-	//
-	// 		break;
-	//
-	// 	case ButtonPress:
-	// 		b =- 1;
-	//
-	// 		if (x_event.xbutton.button == 1) {
-	// 			b = 0;
-	// 		} else if (x_event.xbutton.button == 2) {
-	// 			b = 2;
-	// 		} else if (x_event.xbutton.button == 3) {
-	// 			b = 1;
-	// 		}
-	//
-	// 		if (b >= 0) {
-	// 			mouse_buttonstate |= 1 << b;
-	// 		}
-	//
-	// 		break;
-	//
-	// 	case ButtonRelease:
-	// 		b =- 1;
-	//
-	// 		if (x_event.xbutton.button == 1) {
-	// 			b = 0;
-	// 		} else if (x_event.xbutton.button == 2) {
-	// 			b = 2;
-	// 		} else if (x_event.xbutton.button == 3) {
-	// 			b = 1;
-	// 		}
-	//
-	// 		if (b >= 0) {
-	// 			mouse_buttonstate &= ~(1 << b);
-	// 		}
-	//
-	// 		break;
-	//
-	// 	case ConfigureNotify:
-	// 		// printf("config notify\n");
-	// 		config_notify_width = x_event.xconfigure.width;
-	// 		config_notify_height = x_event.xconfigure.height;
-	// 		config_notify = 1;
-	// 		break;
-	//
-	// 	default:
-	// 		break;
-	// }
 }
 
 // flushes the given rectangles from the view buffer to the screen
@@ -730,23 +350,9 @@ void VID_Update(vrect_t *rects) {
 	}
 }
 
-void Sys_SendKeyEvents(void) {
-	// get events from x server
-	if (oktodraw) {
-		while (SDL_PollEvent(&event)) {
-			// GetEvent();
-			switch(event.type) {
-				case SDL_QUIT:
-					exit(0);
-					break;
-			}
-		}
-
-		while (keyq_head != keyq_tail) {
-			Key_Event(keyq[keyq_tail].key, keyq[keyq_tail].down);
-			keyq_tail = (keyq_tail + 1) & 63;
-		}
-	}
+void VID_Shutdown(void) {
+	Con_Printf("VID_Shutdown\n");
+	SDL_Quit();
 }
 
 void D_BeginDirectRect(int x, int y, byte *pbitmap, int width, int height) {
@@ -755,6 +361,107 @@ void D_BeginDirectRect(int x, int y, byte *pbitmap, int width, int height) {
 
 void D_EndDirectRect(int x, int y, int width, int height) {
 	// direct drawing of the "accessing disk" icon isn't supported under Linux
+}
+
+// ========================================================================
+// SDL Keyboard and Mouse Logic
+// ========================================================================
+
+struct {
+	int key;
+	int down;
+} keyq[64];
+
+int keyq_head = 0;
+int keyq_tail = 0;
+
+int ProcessKey(SDL_Keysym keysym);
+
+void ProcessEvent(void) {
+	int button;
+
+	switch(event.type) {
+		case SDL_QUIT: {
+			Con_Printf("hey thanks for playing have a good day!!!\n");
+			Sys_Quit();
+			break;
+		}
+
+		case SDL_KEYDOWN:
+			keyq[keyq_head].key = ProcessKey(event.key.keysym);
+			keyq[keyq_head].down = true;
+			keyq_head = (keyq_head + 1) & 63;
+			break;
+
+		case SDL_KEYUP:
+			keyq[keyq_head].key = ProcessKey(event.key.keysym);
+			keyq[keyq_head].down = false;
+			keyq_head = (keyq_head + 1) & 63;
+			break;
+
+		case SDL_MOUSEMOTION:
+			mouse_x = (float)event.motion.xrel;
+			mouse_y = (float)event.motion.yrel;
+			break;
+
+		case SDL_MOUSEBUTTONDOWN:
+			button = -1;
+
+			if (event.button.button == 1) {
+				button = 0;
+			} else if (event.button.button == 2) {
+				button = 2;
+			} else if (event.button.button == 3) {
+				button = 1;
+			}
+
+			if (button >= 0) {
+				mouse_buttonstate |= 1 << button;
+			}
+
+			break;
+
+		case SDL_MOUSEBUTTONUP:
+			button = -1;
+
+			if (event.button.button == 1) {
+				button = 0;
+			} else if (event.button.button == 2) {
+				button = 2;
+			} else if (event.button.button == 3) {
+				button = 1;
+			}
+
+			if (button >= 0) {
+				mouse_buttonstate &= ~(1 << button);
+			}
+
+			break;
+
+		// case ConfigureNotify:
+		// 	// printf("config notify\n");
+		// 	config_notify_width = x_event.xconfigure.width;
+		// 	config_notify_height = x_event.xconfigure.height;
+		// 	config_notify = 1;
+		// 	break;
+
+		default:
+			break;
+	}
+}
+
+void Sys_SendKeyEvents(void) {
+	// get events from x server
+	if (oktodraw) {
+		while (SDL_PollEvent(&event)) {
+			ProcessEvent();
+		}
+
+		while (keyq_head != keyq_tail) {
+			Key_Event(keyq[keyq_tail].key, keyq[keyq_tail].down);
+			keyq_tail = (keyq_tail + 1) & 63;
+		}
+	}
 }
 
 void IN_Init(void) {
@@ -837,4 +544,84 @@ void IN_Move(usercmd_t *cmd) {
 	}
 
 	mouse_x = mouse_y = 0.0;
+}
+
+// junk so we don't have to write 26 thousand case statements
+#define CONCAT_H(x,y,z) x##y##z
+#define SINGLEQUOTE '
+#define CONCAT(x,y,z) CONCAT_H(x,y,z)
+#define CHARIFY(x) CONCAT(SINGLEQUOTE , x , SINGLEQUOTE )
+
+#define key_scancode(letter) \
+		case SDL_SCANCODE_##letter: \
+			key = CHARIFY(letter) - 'A' + 'a'; \
+			break
+
+int ProcessKey(SDL_Keysym keysym) {
+	int key = 0;
+
+	switch(keysym.scancode) {
+		case SDL_SCANCODE_LEFT:
+			key = K_LEFTARROW;
+			break;
+
+		case SDL_SCANCODE_RIGHT:
+			key = K_RIGHTARROW;
+			break;
+
+		case SDL_SCANCODE_UP:
+			key = K_UPARROW;
+			break;
+
+		case SDL_SCANCODE_DOWN:
+			key = K_DOWNARROW;
+			break;
+
+		case SDL_SCANCODE_ESCAPE:
+			key = K_ESCAPE;
+			break;
+
+		case SDL_SCANCODE_KP_ENTER:
+		case SDL_SCANCODE_RETURN:
+		case SDL_SCANCODE_RETURN2:
+			key = K_ENTER;
+			break;
+
+		case SDL_SCANCODE_SPACE:
+			key = K_SPACE;
+			break;
+
+		key_scancode(A);
+		key_scancode(B);
+		key_scancode(C);
+		key_scancode(D);
+		key_scancode(E);
+		key_scancode(F);
+		key_scancode(G);
+		key_scancode(H);
+		key_scancode(I);
+		key_scancode(J);
+		key_scancode(K);
+		key_scancode(L);
+		key_scancode(M);
+		key_scancode(N);
+		key_scancode(O);
+		key_scancode(P);
+		key_scancode(Q);
+		key_scancode(R);
+		key_scancode(S);
+		key_scancode(T);
+		key_scancode(U);
+		key_scancode(V);
+		key_scancode(W);
+		key_scancode(X);
+		key_scancode(Y);
+		key_scancode(Z);
+
+
+		default:
+			break;
+	}
+
+	return key;
 }
